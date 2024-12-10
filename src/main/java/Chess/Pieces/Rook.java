1f668aff98900984167779a4f38e00fb7a88b4c7
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2024-12-10 15:26:17 -0300

    feat: Expecional Behavior, signals, constraint, initialy, assignable e helper em rook

--- src/main/java/Chess/Pieces/Rook.java
+++ src/main/java/Chess/Pieces/Rook.java
@@ -5,21 +5,36 @@ import Chess.Move;
 
 public class Rook extends ChessPiece {
 
-    /*@
+	/*@
 	@ requires color == PieceColor.White || color == PieceColor.Black;
 	@ ensures getPieceType() == PieceType.Rook;
 	@ ensures getMoves().length == 4;
 	@ ensures hasRepeatableMoves() == true;
+	@ exceptional_behavior
+	@ 	requires color != PieceColor.White && color != PieceColor.Black;
+	@ signals (IllegalArgumentException e) color != PieceColor.White && color != PieceColor.Black;
 	@*/
 	public Rook(PieceColor color) {
 		super(PieceType.Rook, color, validMoves(), true);
 		//@ assert getPieceType() == PieceType.Rook;
 	}
+	/*@
+	@ initially getPieceType() == PieceType.Rook;
+	@ initially getMoves().length == 4;
+	@ initially hasRepeatableMoves() == true;
+	@ constraint getPieceType() == PieceType.Rook;
+	@ constraint getMoves().length == 4;
+	@ constraint hasRepeatableMoves() == true;
+	@*/
+
+
+
 
     /*@
       @ ensures \result.length == 4;
       @ ensures (\forall int i; 0 <= i && i < \result.length; \result[i] != null);
-	  @ pure
+	  @ assignable \nothing;
+	  @ pure helper
       @*/
     private static Move[] validMoves() {
         return new Move[] {
