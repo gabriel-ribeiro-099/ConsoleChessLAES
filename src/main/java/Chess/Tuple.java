f3e1182efbaab32f4f7294e32640dc6501bcf3f1
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2024-12-11 18:48:00 -0300

    fix: \ e ; de boardmapper

--- src/main/java/Chess/Tuple.java
+++ src/main/java/Chess/Tuple.java
@@ -3,30 +3,51 @@ package Chess;
 /**
  * Used to store an int/int pair to map to tiles on the chessboard.
  */
+/*@
+  @ nullable_by_default
+  @*/
 public class Tuple {
+
     //@ spec_public
     private final int x;
+
     //@ spec_public
     private final int y;
 
-    /*@ public normal_behavior
+    /*@
+      @ public invariant x >= 0 && y >= 0;
+      @ constraint \old(x) == x && \old(y) == y;
+      @*/
+
+    /*@
+      @ public normal_behavior
       @     requires x >= 0;
       @     requires y >= 0;
       @     ensures this.x == x;
       @     ensures this.y == y;
       @*/
-
-    public Tuple(int x, int y){
-            this.x = x;
-            this.y =y;
+    public Tuple(int x, int y) {
+        //@ assert x >= 0 && y >= 0;
+        this.x = x;
+        this.y = y;
     }
 
-    public int X(){
+    /*@
+      @ normal_behavior
+      @ ensures \result == x;
+      @ pure
+      @*/
+    public int X() {
         return x;
     }
 
-    public int Y(){
+    /*@
+      @ normal_behavior
+      @ ensures \result == y;
+      @ pure
+      @*/
+    public int Y() {
         return y;
     }
-
 }
+
