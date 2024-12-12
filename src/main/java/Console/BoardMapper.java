5c3ebfdc4a1adcd0204a9a08de3b466cbb009101
Nicole <cnogueira.nicole@gmail.com>
2024-12-12 17:29:00 -0300

    feat: BoardMapper

--- src/main/java/Console/BoardMapper.java
+++ src/main/java/Console/BoardMapper.java
@@ -15,7 +15,13 @@ class BoardMapper {
               (val == 5 ==> \result == 3) &&
               (val == 6 ==> \result == 2) &&
               (val == 7 ==> \result == 1) &&
-              (val == 8 ==> \result == 0);@*/
+              (val == 8 ==> \result == 0);
+    @ also
+    @ public normal_behavior
+    @     requires val < 1 || val > 8;
+    @     ensures \result == -1;
+    @ pure
+    @*/
     public int map(int val){
         switch(val){
             case 1: return 7;
@@ -26,10 +32,40 @@ class BoardMapper {
             case 6: return 2;
             case 7: return 1;
             case 8: return 0;
+            default: return -1;
         }
-        return -1;
     }
 
+    /*@ public normal_behavior
+      @     requires Character.toLowerCase(val) == 'a' ||
+      @              Character.toLowerCase(val) == 'b' ||
+      @              Character.toLowerCase(val) == 'c' ||
+      @              Character.toLowerCase(val) == 'd' ||
+      @              Character.toLowerCase(val) == 'e' ||
+      @              Character.toLowerCase(val) == 'f' ||
+      @              Character.toLowerCase(val) == 'g' ||
+      @              Character.toLowerCase(val) == 'h';
+      @     ensures (Character.toLowerCase(val) == 'a' ==> \result == 0) &&
+      @             (Character.toLowerCase(val) == 'b' ==> \result == 1) &&
+      @             (Character.toLowerCase(val) == 'c' ==> \result == 2) &&
+      @             (Character.toLowerCase(val) == 'd' ==> \result == 3) &&
+      @             (Character.toLowerCase(val) == 'e' ==> \result == 4) &&
+      @             (Character.toLowerCase(val) == 'f' ==> \result == 5) &&
+      @             (Character.toLowerCase(val) == 'g' ==> \result == 6) &&
+      @             (Character.toLowerCase(val) == 'h' ==> \result == 7);
+      @ also
+      @ public normal_behavior
+      @     requires !(Character.toLowerCase(val) == 'a' ||
+      @               Character.toLowerCase(val) == 'b' ||
+      @               Character.toLowerCase(val) == 'c' ||
+      @               Character.toLowerCase(val) == 'd' ||
+      @               Character.toLowerCase(val) == 'e' ||
+      @               Character.toLowerCase(val) == 'f' ||
+      @               Character.toLowerCase(val) == 'g' ||
+      @               Character.toLowerCase(val) == 'h');
+      @     ensures \result == -1;
+      @ pure
+      @*/
     public int map(char val){
         switch(Character.toLowerCase(val)){
             case 'a': return 0;
@@ -40,7 +76,7 @@ class BoardMapper {
             case 'f': return 5;
             case 'g': return 6;
             case 'h': return 7;
+            default: return -1;
         }
-        return -1;
     }
 }
