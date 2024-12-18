c6a52b63bef229f26ae40a9d14a261b2dada09fe
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2024-12-17 23:23:06 -0300

    fix:11 verifys em BoardDisplay

--- src/main/java/Console/BoardDisplay.java
+++ src/main/java/Console/BoardDisplay.java
@@ -14,30 +14,28 @@ public class BoardDisplay {
     @                  board.getBoardArray()[i][j] != null));
     @ ensures true;
     @*/
-    public static void printBoard(ChessBoard board){
+    public static void printBoard(ChessBoard board) {
         clearConsole();
         Tile[][] b = board.getBoardArray();
+
+        //@ assert b != null && b.length == 8;
+        //@ assert (\forall int i; 0 <= i && i < b.length;
+        //@            b[i] != null && b[i].length == 8 &&
+        //@            (\forall int j; 0 <= j && j < b[i].length; b[i][j] != null));
 
         System.out.println();
         System.out.println("      [A][B][C][D][E][F][G][H] \n");
 
         //@ maintaining 0 <= i && i <= 8;
-        //@ maintaining (\forall int k; 0 <= k && k < i;
-        //@                (\forall int j; 0 <= j && j < 8; b[k][j] != null));
-        // loop_writes i, System.out.outputText, System.out.eol;
         //@ decreases 8 - i;
-        for(int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {
+            //@ assert b[i] != null && b[i].length == 8;
             System.out.print("[" + (8 - i) + "]   ");
 
             //@ maintaining 0 <= j && j <= 8;
-            //@ maintaining (\forall int k; 0 <= k && k < j; b[i][k] != null);
-            //@ maintaining b[i] != null && b[i].length == 8;
-            // loop_writes j, System.out.outputText;
             //@ decreases 8 - j;
-            for (int j = 0; j < 8; j++){
-                //@ assert 0 <= i && i < 8;
-                //@ assert 0 <= j && j < 8;
-                //@ assert b[i] != null && b[i][j] != null; 
+            for (int j = 0; j < 8; j++) {
+                //@ assert b[i][j] != null;
                 System.out.print(b[i][j].getValue());
             }
 
